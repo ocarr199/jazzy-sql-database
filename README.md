e631d97ba754b4d0abce39fe93f074f71f6a0a0c
Owen Carruth <ocarr199@gmail.com>
2021-05-25 19:46:22 -0500

    BOTH GETS

--- README.md
+++ README.md
@@ -30,16 +30,16 @@ From this ERD, we can see that we'll need to setup 2 tables: artist & song. We c
 
 Using the ERD above:
 
-- [ ] Create a new database called `jazzy_sql` using Postico.
-- [ ] Write the SQL to create both of the tables (Note: Make sure to use the table names show.)
+- [x] Create a new database called `jazzy_sql` using Postico.
+- [x] Write the SQL to create both of the tables (Note: Make sure to use the table names show.)
 - [ ] Write INSERT statements to add the sample data provided in the `server.js` file. 
 - [ ] Create the tables and insert the test data using Postico.
 - [ ] Add the SQL to the `database.sql` file. This file will help others interested in this project, get it setup correctly if they pick it up from GitHub.
 
 ### Update the Server-side Code
 
-- [ ] Install `pg` and setup your `pool` to connect to the database.
-- [ ] Update the `\artist` GET route to get data from the database. Sort the data by birthdate, youngest to oldest.
+- [x] Install `pg` and setup your `pool` to connect to the database.
+- [x ] Update the `\artist` GET route to get data from the database. Sort the data by birthdate, youngest to oldest.
 - [ ] Update the `\artist` POST route to store data in the database. 
 - [ ] Update the `\song` GET route to get data from the database. Sort the data by title.
 - [ ] Update the `\song` POST route to store data in the database.  
@@ -48,6 +48,6 @@ Using the ERD above:
 
 ## Stretch
 
-- [ ] Move the `pool` configuration out of `server.js` and into a `pool.js` module file.
+- [x ] Move the `pool` configuration out of `server.js` and into a `pool.js` module file.
 - [ ] Move the `\artist` routes to an artist router file. 
 - [ ] Move the `\song` routes to a song router file.
--- server/server.js
+++ server/server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
 const PORT = 5000;
+const pg = require('pg');
+const Pool = pg.Pool;
+const pool = new Pool({
+    database: 'jazzy_sql', // the name of database, This can change!
+    host: 'localhost', // where is your database?
+    port: 5432 // the port for your database, 5432 is default for postgres
+});
+
+pool.on('connect', () => {
+    console.log('connected to pg')
+})
+
+pool.on('err', (err) => {
+    console.log(err)
+})
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
@@ -12,24 +26,25 @@ app.listen(PORT, () => {
 });
 
 // TODO - Replace static content with a database tables
-const artistList = [ 
-    {
-        name: 'Ella Fitzgerald',
-        birthdate: '04-25-1917'
-    },
-    {
-        name: 'Dave Brubeck',
-        birthdate: '12-06-1920'
-    },       
-    {
-        name: 'Miles Davis',
-        birthdate: '05-26-1926'
-    },
-    {
-        name: 'Esperanza Spalding',
-        birthdate: '10-18-1984'
-    },
-]
+
+// const artistList = [ 
+//     {
+//         name: 'Ella Fitzgerald',
+//         birthdate: '04-25-1917'
+//     },
+//     {
+//         name: 'Dave Brubeck',
+//         birthdate: '12-06-1920'
+//     },       
+//     {
+//         name: 'Miles Davis',
+//         birthdate: '05-26-1926'
+//     },
+//     {
+//         name: 'Esperanza Spalding',
+//         birthdate: '10-18-1984'
+//     },
+// ]
 const songList = [
     {
         title: 'Take Five',
@@ -48,11 +63,27 @@ const songList = [
     }
 ];
 
+// app.get('/artist', (req, res) => {
+//     console.log(`In /songs GET`);
+//     res.send(artistList);
+// });
+
+// SQL
+
 app.get('/artist', (req, res) => {
-    console.log(`In /songs GET`);
-    res.send(artistList);
+    let queryText = `SELECT * FROM "artist" ORDER BY "birthdate" DESC;`;
+    pool.query(queryText)
+        .then((result) => {
+            console.log(result)
+            res.send(result.rows);
+        })
+        .catch((err) => {
+            console.log(`Error making query ${queryText}`, err);
+            res.sendStatus(500);
+        });
 });
 
+// SQL DONE
 app.post('/artist', (req, res) => {
     artistList.push(req.body);
     res.sendStatus(201);
@@ -63,9 +94,17 @@ app.get('/song', (req, res) => {
     res.send(songList);
 });
 
-app.post('/song', (req, res) => {
-    songList.push(req.body);
-    res.sendStatus(201);
+app.get('/song', (req, res) => {
+    let queryText = `SELECT * FROM "artist" ORDER BY "birthdate" DESC;`;
+    pool.query(queryText)
+        .then((result) => {
+            console.log(result)
+            res.send(result.rows);
+        })
+        .catch((err) => {
+            console.log(`Error making query ${queryText}`, err);
+            res.sendStatus(500);
+        });
 });
 
 
